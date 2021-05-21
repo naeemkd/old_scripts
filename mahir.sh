0f083f49a5c598de504145d21b5686b523a430b7
N Khan <n256@vivaldi.net>
2021-05-21 21:59:42 +0000

    Update mahir.sh

--- mahir.sh
+++ mahir.sh
@@ -7,12 +7,18 @@ shopt -s nocasematch
 
 case "$subm_user" in
 	"")
-		echo "No name specified." && exit 2 ;;
+		printf "No name specified.\n" 1>&2
+		exit 2 ;;
 	mahir|silver)
-		printf "\e[1;31mGo away\n" && exit 1 ;;
+		printf "\e[1;31mGo away\n" 1>&2
+		exit 1 ;;
 	*)
-		if dialog --yesno "Are you sure you would like to continue?" 10 50
-			then clear && printf "Silver's real name is \e[1;1mMahir\n"
-			else clear && echo "you scared nub?" && exit 1
+		if dialog --yesno "Are you sure you would like to continue?" 10 50; then 
+			clear
+			timeout 10 yes "SILVER ALWAYS GETS PWNED"
+		else 
+				clear
+				printf "you scared nub?\n" 1>&2
+				exit 1
 		fi ;;
 esac
